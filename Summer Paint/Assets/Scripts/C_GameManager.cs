6e09051be27da6f55895f1c7abe64b3d45613ffd
kcaybasi <k.furkancaybasi@gmail.com>
2022-04-17 18:36:31 +0300

    1.0.5-Juice spawning added.

--- Summer Paint/Assets/Scripts/C_GameManager.cs
+++ Summer Paint/Assets/Scripts/C_GameManager.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 
 public class C_GameManager : MonoBehaviour
 {
+
+    private Animator gameManagerAnimator;
 
     [Header("UI Menus")]
     [SerializeField] GameObject mainMenu;
@@ -12,11 +15,20 @@ public class C_GameManager : MonoBehaviour
     [SerializeField] GameObject gameOverMenu;
     List<GameObject> menuList = new List<GameObject>();
 
+    [Header("Juice Holders")]
+    [SerializeField] List<GameObject> juiceList = new List<GameObject>();
+    public GameObject selectedJuice;
 
-    private Animator gameManagerAnimator;
+    public static C_GameManager instance;
+
+    private void Awake()
+    {
+        instance = this;
+    }
 
     private void Start()
     {
+        DOTween.Init();
         AddMenuObjectsToList();
         gameManagerAnimator = GetComponent<Animator>();
     }
@@ -28,7 +40,6 @@ public class C_GameManager : MonoBehaviour
         menuList.Add(gameplayMenu);
         menuList.Add(gameOverMenu);
     }
-
 
     private void OpenMenu(GameObject menuGameObj)
     {
@@ -45,11 +56,43 @@ public class C_GameManager : MonoBehaviour
 
     }
 
+    private void ActivateSelectedJuice(GameObject juiceObj)
+    {
+        int selectedJuiceIndex = juiceList.IndexOf(juiceObj);
+        for(int i = 0; i < juiceList.Count; i++)
+        {
+            if (i != selectedJuiceIndex)
+            {
+                juiceList[i].SetActive(false);
+            }
+        }
+        juiceObj.SetActive(true);
+        selectedJuice = juiceObj;
+        selectedJuice.transform.DOShakePosition(0.8f, 0.4f, 6, 10);
+        
+        
+    }
+
     // Button functions 
 
     public void StartMakingPopsticle()
     {
         gameManagerAnimator.SetBool("MakingPopsicle", true); //Trigger state camera turning 
         OpenMenu(gameplayMenu);
+    }
+
+    public void SelectStrawberryJuice()
+    {
+        ActivateSelectedJuice(juiceList[0]);
+    }
+
+    public void SelectKiwiJuice()
+    {
+        ActivateSelectedJuice(juiceList[1]);
+    }
+
+    public void SelectOrangeJuice()
+    {
+        ActivateSelectedJuice(juiceList[2]);
     }
 }
--- Summer Paint/Assets/Scripts/Player.cs
+++ Summer Paint/Assets/Scripts/Player.cs
@@ -1,8 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
+
 
 public class Player : MonoBehaviour
 {
+    private GameObject selectedJuice;
 
+
+    private void GetSelectedJuice()
+    {
+        selectedJuice = C_GameManager.instance.selectedJuice;
+    }
+
+    private void Update()
+    {
+        
+    }
 }
